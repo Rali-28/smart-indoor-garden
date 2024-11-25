a365df99e296f9d2818ee1c2b036f8a54b68b257
Rali <113418936+Rali-28@users.noreply.github.com>
2024-11-26 02:59:09 +0800

    Add blynk device wifi provisioning function
    Necessary activation codes are commented for later use.
    Will temporarily use hardcoded wifi autheticantion.

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,20 @@
 #include <Arduino.h>
 #include <BlynkSimpleEsp32.h>
 
-void setup() {
+// * Blynk Device Wifi Provisioning, For use later
+/**
+ * #define USE_ESP32_DEV_MODULE
+ * #include "BlynkEdgent.h"
+ */
 
+void setup()
+{
+  // * Blynk Wifi Provisioning
+  // BlynkEdgent.begin();
 }
 
-void loop() {
-
+void loop()
+{
+  // * Blynk Wifi Provisioning
+  // BlynkEdgent.run();
 }
